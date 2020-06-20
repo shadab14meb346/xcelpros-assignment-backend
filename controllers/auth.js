11b5d0879ff1909acc30dcd9877a38d795874f59
Shadab <shadab14meb346@gmail.com>
2020-06-20 20:39:27 +0530

    Feat:register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,29 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
-exports.test = asyncHandler(async (req, res, next) => {
-  res.send('test');
+// @desc      Register user
+// @route     POST /auth/register
+// @access    Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  sendTokenResponse(user, 200, res);
 });
+
+// Get token from model and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(statusCode).json({
+    success: true,
+    token,
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { test } = require('../controllers/auth');
+const { register } = require('../controllers/auth');
 
 const router = express.Router();
 
 const { protect } = require('../middleware/auth');
 
-router.get('/test', test);
+router.post('/register', register);
+
 module.exports = router;
