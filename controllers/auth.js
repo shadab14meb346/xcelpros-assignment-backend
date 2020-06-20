7ba215da535325ab168636bf3bb040f3238eed56
Shadab <shadab14meb346@gmail.com>
2020-06-20 20:42:43 +0530

    Feat:login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,6 +18,34 @@ exports.register = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc      Login user
+// @route     POST /auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  sendTokenResponse(user, 200, res);
+});
+
 // Get token from model and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
